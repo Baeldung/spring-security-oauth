6bd09240dd163d0927da4857e3a68c65b35320a4
Omkar A <omkara.adg@gmail.com>
2025-03-19 16:27:01 +0530

    -- redis auth server code

---
+++ redis-authorization-server/redis-oauth-server/src/main/java/com/baeldung/redis/RedisOAuth2AuhorizationServerApplication.java
@@ -0,0 +1,12 @@
+package com.baeldung.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisOAuth2AuhorizationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisOAuth2AuhorizationServerApplication.class, args);
+    }
+}
